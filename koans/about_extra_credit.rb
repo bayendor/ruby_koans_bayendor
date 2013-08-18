a3910f18c19b75e10d9da35bfda773d7766755be
David Bayendor <dbayendor@gmail.com>
2013-08-18 11:07:35 -0600

    Fix about_extra_credit koan
    * Add class & methods from dice and greed project
    
    [#54145711]

--- koans/about_extra_credit.rb
+++ koans/about_extra_credit.rb
@@ -6,3 +6,33 @@
 # You already have a DiceSet class and score function you can use.
 # Write a player class and a Game class to complete the project.  This
 # is a free form assignment, so approach it however you desire.
+
+class DiceSet
+  def roll(num_of_rolls)
+    @dice_array = []
+    num_of_rolls.times { @dice_array.push(1 + rand(6)) }
+  end
+
+  def values
+    @dice_array
+  end
+end
+
+def score(dice)
+  sum = 0
+  (1..6).each do |rolls|
+    die = dice.select { |die_value| die_value == rolls }
+    if die.size >= 3
+      sum += (rolls==1 ? 1000 : rolls*100)
+    end
+    if rolls == 1
+      sum += (die.size % 3) * 100
+    end
+    if rolls == 5
+      sum += (die.size % 3) *  50
+    end
+  end
+  sum
+end
+
+
